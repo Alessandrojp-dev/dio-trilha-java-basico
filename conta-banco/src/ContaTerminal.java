386c8052676e7a3540ea13c9635866bb7adef96d
Alessandrojp-dev <alessandrojp_dev@hotmail.com>
2025-07-16 21:02:45 -0300

    Desafio conta banco pelo terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+ 
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = sc.nextLine();
+
+        System.out.println("Digite o número da Conta !");
+        int conta = sc.nextInt();
+
+        System.out.println("Digite seu nome completo: ");
+        sc.nextLine(); 
+        String nome = sc.nextLine();
+
+        System.out.println("Agora, deposite um valor para criar sua conta !");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+
+        sc.close();
     }
 }
